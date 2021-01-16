e8f63e4b93c0b0faed302df69705cfb970f35e79
OhYoooo <32889561+OhYoooo@users.noreply.github.com>
2021-01-16 18:45:36 -0500

    update sample test cases

--- Function/sample/sample_request.js
+++ Function/sample/sample_request.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const request = require('request');
 
-const fileName = 'test.png' /* modify your test_name here */
+const fileName = 'test_.png' /* modify your test_name here */
 const imageBuffer = fs.readFileSync(fileName); /* has to be base64 encoded string */
 
 const options = {
