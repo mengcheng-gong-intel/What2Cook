7c56ce61d5ef8f8770901d4242306e29100253f6
OhYoooo <32889561+OhYoooo@users.noreply.github.com>
2021-01-16 06:32:50 -0500

    add autoML client

--- Function/index.js
+++ Function/index.js
@@ -1,8 +1,15 @@
+const { predict } = require('./src/predict');
+
 /**
  * Send "Hello, World!"
  * @param req https://expressjs.com/en/api.html#req
  * @param res https://expressjs.com/en/api.html#res
  */
 exports.What2CookFunction = (req, res) => {
-    res.send('Hello, What2Cook!');
+    res.status(200).send('Hello, What2Cook!');
+};
+
+exports.Predict = async (req, res) => {
+    const result = await predict(req.payload);
+    res.status(200).send(result);
 };
---
+++ Function/src/predict.js
@@ -0,0 +1,40 @@
+const projectId = 'what2cook-301906';
+const location = 'us-central1';
+const modelId = 'YOUR_MODEL_ID';
+
+// Imports the Google Cloud AutoML library
+const {PredictionServiceClient} = require('@google-cloud/automl').v1;
+
+// Instantiates a client
+const client = new PredictionServiceClient();
+
+const predict = async (content) => {
+    // Construct request
+    // params is additional domain-specific parameters.
+    // score_threshold is used to filter the result
+    const request = {
+        name: client.modelPath(projectId, location, modelId),
+        payload: {
+            image: {
+                imageBytes: content,
+            },
+        },
+    };
+
+    const [response] = await client.predict(request);
+
+    const result = response.payload.map((annotationPayload) => {
+        return annotationPayload.displayName;
+    });
+
+    // for (const annotationPayload of response.payload) {
+    //     console.log(`Predicted class name: ${annotationPayload.displayName}`);
+    //     console.log(
+    //     `Predicted class score: ${annotationPayload.classification.score}`
+    //     );
+    // }
+
+    return result
+}
+
+module.exports.predict = predict;
