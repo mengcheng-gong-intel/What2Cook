f40c2cdb82ce982f5e499f39946fc0cf1a08e2d9
OhYoooo <32889561+OhYoooo@users.noreply.github.com>
2021-01-16 23:34:43 -0500

    add endpoint to fetch images

--- Function/index.js
+++ Function/index.js
@@ -1,7 +1,8 @@
 const { predict } = require('./src/predict');
+const request = require('request');
 
 /**
- * Send "Hello, World!"
+ * Health check
  * @param req https://expressjs.com/en/api.html#req
  * @param res https://expressjs.com/en/api.html#res
  */
@@ -9,14 +10,51 @@ exports.What2CookFunction = (req, res) => {
     res.status(200).send('Hello, What2Cook!');
 };
 
+/**
+ * Send image to AutoML and return prediction
+ */
 exports.Predict = async (req, res) => {
     const result = await predict(req.body);
     res.status(200).send(result);
 };
 
-exports.Test = async (req, res) => {
-    console.log(req.body)
-    img64 = Buffer.from(req.body).toString('base64')
-    // res.status(200).send(img64);
-    res.status(200).send(req.body);
+/**
+ * Fetch image, return 7.52x7.52 inches jpg image
+ */
+exports.FetchImage = async (req, res) => {
+    const path = req.body.imageUri;
+    request.get(path, { encoding: null }, (err,resp) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);
+        } else {
+            if(resp.statusCode !== 200 ) {
+                console.log(resp);
+            } else {
+                res.contentType(resp.headers['content-type']);
+                res.end(resp.body, 'binary');
+            }
+        }
+    });
 };
+
+/**
+ * Test for local, run ./run.sh Test local
+ */
+exports.Test = async (req, res) => {
+    const path = 'https://storage.googleapis.com/what2cook/15130468796375440_460_380.jpg'
+
+    request.get(path, { encoding: null }, (err,resp) => {
+        if (err) {
+            console.log(err)
+        } else {
+            if(resp.statusCode !== 200 ) {
+                console.log(resp)
+            } else {
+                res.contentType(resp.headers['content-type']);
+                res.end(resp.body, 'binary');
+            }
+        }
+    });
+};
+
