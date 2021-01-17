8708837838494a620e2bd922a41691761407055f
Gmcccccc <gmc2777522@outlook.com>
2021-01-17 01:38:04 -0500

    DB side code updated

--- Scripts/CRDB_Cluster_init_local.sh
+++ Scripts/CRDB_Cluster_init_local.sh
@@ -7,11 +7,11 @@ cockroach start --insecure --store=node2 --listen-addr=localhost:26258 --http-ad
 echo starting node3
 cockroach start --insecure --store=node3 --listen-addr=localhost:26259 --http-addr=localhost:8082 --join=localhost:26257,localhost:26258,localhost:26259 --background
 
-echo starting node4
-cockroach start --insecure --store=node4 --listen-addr=localhost:26260 --http-addr=localhost:8083 --join=localhost:26257,localhost:26258,localhost:26259 --background
+# echo starting node4
+# cockroach start --insecure --store=node4 --listen-addr=localhost:26260 --http-addr=localhost:8083 --join=localhost:26257,localhost:26258,localhost:26259 --background
 
-echo starting node5
-cockroach start --insecure --store=node5 --listen-addr=localhost:26261 --http-addr=localhost:8084 --join=localhost:26257,localhost:26258,localhost:26259 --background
+# echo starting node5
+# cockroach start --insecure --store=node5 --listen-addr=localhost:26261 --http-addr=localhost:8084 --join=localhost:26257,localhost:26258,localhost:26259 --background
 
 echo initialize the cluster...
 cockroach init --insecure --host=localhost:26257
